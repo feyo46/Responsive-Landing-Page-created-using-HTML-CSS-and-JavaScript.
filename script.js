7008fa6b511f839a0718794fe3357c416e60c8d1
feyo46 <feyisaasefa46@gmail.com>
2025-04-06 13:53:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll navbar effect
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  navbar.classList.toggle('scrolled', window.scrollY > 50);
+});
